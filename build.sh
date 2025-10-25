c3471a8bf73b635e03687338d893374e9c8a923a
ralph <github@ralph-schuster.eu>
2025-10-25 15:15:15 +0000

    Multi-arch container build

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-docker build -t technicalguru/mailserver-roundcube:latest .
+docker buildx build \
+     --progress=plain \
+     -t technicalguru/mailserver-roundcube:latest \
+     -t technicalguru/mailserver-roundcube:1.6.11.1 \
+     -t technicalguru/mailserver-roundcube:1.6.11 \
+     -t technicalguru/mailserver-roundcube:1.6 \
+     -t technicalguru/mailserver-roundcube:1 \
+     --push \
+     --platform linux/amd64,linux/arm64 \
+
+#docker build -t technicalguru/mailserver-roundcube:latest .
